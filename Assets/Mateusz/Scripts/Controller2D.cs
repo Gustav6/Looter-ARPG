1588e7eca12a52490e9541cd9554535f0f32bcfb
SkieoXDD <119525838+SkieoXDD@users.noreply.github.com>
2024-09-17 12:09:32 +0200

    best commit the trilogy
    fixed collisions
    broke move speed :sunglasses:

--- Assets/Mateusz/Scripts/Controller2D.cs
+++ Assets/Mateusz/Scripts/Controller2D.cs
@@ -1,56 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-[RequireComponent(typeof(CapsuleCollider2D))]
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
+
     public LayerMask collisionMask;
 
     const float skinWidth = .015f;
-    public int horizontalRays = 4;
-    public int verticalRays = 4;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
 
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
-
-    CapsuleCollider2D col;
+    BoxCollider2D collider;
     RaycastOrigins raycastOrigins;
+
     void Start()
     {
-        col = GetComponent<CapsuleCollider2D>();
+        collider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
     }
 
     public void Move(Vector3 velocity)
     {
         UpdateRaycastOrigins();
-
         if (velocity.x != 0)
         {
-            HorizontalCollision(ref velocity);
+            HorizontalCollisions(ref velocity);
         }
-
         if (velocity.y != 0)
         {
-            VerticalCollision(ref velocity);
+            VerticalCollisions(ref velocity);
         }
-      
+
         transform.Translate(velocity);
     }
 
-    void HorizontalCollision(ref Vector3 velocity)
+    void HorizontalCollisions(ref Vector3 velocity)
     {
         float directionX = Mathf.Sign(velocity.x);
         float rayLength = Mathf.Abs(velocity.x) + skinWidth;
-        for (int i = 0; i < horizontalRays; i++)
+
+        for (int i = 0; i < horizontalRayCount; i++)
         {
-            Vector2 rayOrigin = (directionX == -1) ? rayOrigin = raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
-            rayOrigin += Vector2.right * (verticalRaySpacing * i);
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
 
-            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.blue);
 
             if (hit)
             {
@@ -60,17 +60,18 @@ public class Controller2D : MonoBehaviour
         }
     }
 
-    void VerticalCollision(ref Vector3 velocity)
+    void VerticalCollisions(ref Vector3 velocity)
     {
         float directionY = Mathf.Sign(velocity.y);
         float rayLength = Mathf.Abs(velocity.y) + skinWidth;
-        for (int i = 0; i < verticalRays; i++)
+
+        for (int i = 0; i < verticalRayCount; i++)
         {
-            Vector2 rayOrigin = (directionY == -1) ? rayOrigin = raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
 
-            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.blue);
 
             if (hit)
             {
@@ -82,26 +83,25 @@ public class Controller2D : MonoBehaviour
 
     void UpdateRaycastOrigins()
     {
-        Bounds bounds = col.bounds;
-        bounds.Expand(skinWidth * -2);
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * 1);
 
         raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.max.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
         raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
         raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
     }
 
-
     void CalculateRaySpacing()
     {
-        Bounds bounds = col.bounds;
+        Bounds bounds = collider.bounds;
         bounds.Expand(skinWidth * -2);
 
-        horizontalRays = Mathf.Clamp(horizontalRays, 2, int.MaxValue);
-        verticalRays = Mathf.Clamp(verticalRays, 2, int.MaxValue);
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
 
-        horizontalRaySpacing = bounds.size.y / (horizontalRays - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRays - 1);
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
     }
 
     struct RaycastOrigins
@@ -109,4 +109,5 @@ public class Controller2D : MonoBehaviour
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
+
 }
--- Assets/Mateusz/Scripts/Player.cs
+++ Assets/Mateusz/Scripts/Player.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    float moveSpeed = 6;
+    float walkSpeed = 5;
+    float runSpeed = 25f;
+   public float currentSpeed;
+   public float stamina, staminaMax;
+   public float runCost;
+   public   float chargeRate;
+
+    bool running;
+
+
+    public Image staminaBar;
+
+    private Coroutine recharge;
+    
 
     Vector3 velocity;
-    Vector2 direction;
 
     Controller2D controller;
 
@@ -18,14 +31,59 @@ public class Player : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<Controller2D>();
+        Debug.Log(+currentSpeed);
     }
 
     private void Update()
     {
-        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        velocity = direction.normalized * moveSpeed;
+        currentSpeed = walkSpeed;
+
+        velocity.x = direction.x * currentSpeed;
+        velocity.y = direction.y * currentSpeed;
         controller.Move(velocity * Time.deltaTime);
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && stamina > 0)
+        {
+            running = true;
+
+        }else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            running = false;
+        }
+
+        if (running)
+        {
+            currentSpeed = runSpeed;
+            stamina -= runCost * Time.deltaTime;
+            if (stamina < 0) 
+            {
+                stamina = 0;
+                staminaBar.fillAmount = stamina / staminaMax;
+            }
+            StopCoroutine(recharge);
+        }
+        else if (!running)
+        {
+            currentSpeed = walkSpeed;
+            recharge = StartCoroutine(rechargeStamina());
+        }
+    }
+
+    private IEnumerator rechargeStamina()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while (stamina < staminaMax)
+        {
+            stamina += chargeRate / 10f;
+            if (stamina > staminaMax)
+            {
+                stamina = staminaMax;
+                staminaBar.fillAmount = stamina / staminaMax;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
     }
 }

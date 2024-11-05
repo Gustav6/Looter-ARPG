2adf7c9167b0487ed2b90a38f186bb1f2d4149f4
kidnamedfinger2 <andrej.gazivoda@elev.ga.lbs.se>
2024-11-05 11:28:39 +0100

    Enemy interface done

--- Assets/Andrej/Scripts/EnemyProperties.cs
+++ Assets/Andrej/Scripts/EnemyProperties.cs
@@ -2,27 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyProperties : MonoBehaviour
+public class EnemyProperties : MonoBehaviour, IDamagable
 {
     public GameObject player;
     public float distanceToPlayer;
 
     public float speed = 10f;
-    public float health = 100f;
+
+    private int health;
+    public int CurrentHealth
+    {
+        get => health; set
+        {
+            if (value > MaxHealth)
+            {
+                health = MaxHealth;
+            }
+            else if (value <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                health = value;
+            }
+        } 
+    }
+    [field: SerializeField] public int MaxHealth { get; set; }
 
 
     private void Start()
     {
+        CurrentHealth = MaxHealth;
+
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
-        if (health <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    public void Damage(int damageAmount)
+    {
+        CurrentHealth -= damageAmount;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }

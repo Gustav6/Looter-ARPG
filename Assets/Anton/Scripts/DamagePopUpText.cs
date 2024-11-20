abe0bdb833d58804564adbf017cff6dea0111424
ANDOBRUH <122355264+ANDOBRUH@users.noreply.github.com>
2024-11-20 21:43:44 +0100

    AHH AHHH AAHHHH

--- Assets/Anton/Scripts/DamagePopUpText.cs
+++ Assets/Anton/Scripts/DamagePopUpText.cs
@@ -19,17 +19,23 @@ public class DamagePopUpText : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = new Vector2(Random.Range(-0.2f, 0.2f), 0.7f) * moveSpeed;
 
-        TransitionSystem.AddTransition(new ScaleTransition(transform, 0.5f, new Vector3(1.5f, 1.5f), TransitionType.SmoothStop2), gameObject);
+        TransitionSystem.AddTransition(new ScaleTransition(transform, 0.5f, new Vector3(1.5f, 1.5f), TransitionType.SmoothStop2, ShrinkAfterFirstTransiton), gameObject); 
     }
 
     private void Update()
     {
         rb.linearVelocity -= new Vector2(0, 0.3f);
         timer -= Time.deltaTime;
+
         if (timer <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void ShrinkAfterFirstTransiton()
+    {     
+       TransitionSystem.AddTransition(new ScaleTransition(transform, 0.5f, new Vector3(.1f, .1f), TransitionType.SmoothStart2), gameObject);
     }
 }
     

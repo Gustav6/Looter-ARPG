12bf4419c0a3a8382eb1aac9f7543529348bd176
Gustav Hemma <gustav.ekstrom@elev.ga.lbs.se>
2024-11-24 23:52:51 +0100

    Added loot "movement"

--- Assets/Gustav/Scripts/LootScript.cs
+++ Assets/Gustav/Scripts/LootScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class LootScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Start()
+    {
+        Vector2 t = new (Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+        rb.AddForce(t * 150, ForceMode2D.Impulse);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
